eed7a25050acc3ec8f5a89ea0c9834019300551a
mdrihan78 <mdrihanrihan89@gmail.com>
2025-06-29 23:48:33 +0600

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,133 @@
+package accountsystems2;
+
+public class Account {
+    private String id;
+    private String name;
+    private double balance;
+    private int type;
+    private double interest;
+    private int installments=0;
+    private double srate=0.025;
+    private double drate=0.07;
+    private String typString;
+    public Account(String id,String name,int type,double balance){
+        this.id=id;
+        this.name=name;
+        this.balance=balance;
+        this.type=type;
+        if(this.type==1){
+            typString="Regular";
+        }
+        else if(this.type==2){
+            typString="Savings";
+        }
+        else if(this.type==3){
+            typString="Deposit";
+        }
+        this.installments++;
+    }
+    public Account(String id,String name,int type){
+        this.id=id;
+        this.name=name;
+        this.type=type;
+        if(this.type==1){
+            typString="Regular";
+        }
+        else if(this.type==2){
+            typString="Savings";
+        }
+        else if(this.type==3){
+            typString="Deposit";
+        }
+        this.balance=0.0;
+    }
+    public String getID(){
+        return this.id;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public double getInterest(){
+         
+         double temp = 0;
+         if(this.type==1) temp=0;
+         else if(this.type==2){
+            temp=(this.balance)*srate;
+            this.balance+=(this.balance)*srate;
+         }
+         else if(this.type==3){
+             temp=(this.balance)*drate;
+            this.balance+=(this.balance)*drate;
+         }
+         return temp;
+    }
+    public double getBal(){
+        return this.balance;
+    }
+    public void withdraw(double amount){
+        if(amount<=this.balance){
+            switch(this.type){
+                case 1: 
+                this.balance-=amount;
+                 System.out.println("\nWithdrawl finished !\n");
+                 break;
+                 case 2:
+                 if(this.balance<1000){
+                    System.out.println("Can not withdraw less than 1000 !");
+                 }
+                 else{
+                     this.balance-=amount;
+                      System.out.println("\nWithdrawl finished !\n");
+                 }
+                 break;
+                 case 3:
+                  if(installments<5){
+                     System.out.println("Can not withdraw less than 5 installments !");
+                  }
+                  else{
+                    this.balance-=amount;
+                     System.out.println("\nWithdrawl finished !\n");
+                    installments=0;
+                  }
+                  break;
+                  default: 
+                   break;
+            }
+        }
+        else{
+            System.out.println("NOT enough balance !");
+
+        }
+        
+    }
+    public void deposit(double amount){
+        if(amount<=0){
+            System.out.println("Invalid deposit amount");
+         return;
+        }
+        if(this.type==3) installments++;
+        this.balance+=amount;
+         System.out.println("\nDeposit finished !\n");
+    }
+    public void transfer(Account accdes,double amount){
+        if(amount<=this.balance && this.type!=3){
+            this.balance-=amount;
+            accdes.balance+=amount;
+            System.out.println("\nTransfer finished !\n");
+        }
+        else{
+            if(amount>this.balance) {
+                System.out.println("Not enough balance");
+            }
+            if(this.type==3){
+                System.out.println("Cannot transfer in deposit account");
+            }
+        }
+       
+    }
+    public String toString(){
+       
+        return "name: "+this.name+"\nid: "+this.id+"\nbalance: "+this.balance+"\ntype :"+this.typString;
+        
+    }
+}
---
+++ main.java
@@ -0,0 +1,140 @@
+package accountsystems2;
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args){
+        Account[] accounts=new Account[200];
+        Scanner scan = new Scanner(System.in);
+        int count =0; 
+        int choice = -1;
+    
+         while(true){
+            String name;
+            String id;
+            double balance;
+            double amount;
+            Account acc;
+           System.out.println("1) Create Account");
+           System.out.println("2) Deposit");
+           System.out.println("3) Withdraw");
+           System.out.println("4) Transfer Money");
+           System.out.println("5) Show all");
+           System.out.println("6) Interests");
+           System.out.println("7) EXIT");
+           System.out.print("Enter Choice :");
+           choice =  scan.nextInt();
+           scan.nextLine();
+           switch(choice){
+            case 1: 
+                 System.out.print("Enter your name:");
+                 name= scan.nextLine();
+                 System.out.print("Enter your id:");
+                id= scan.nextLine();
+                System.out.println("Enter Account type --");
+                System.out.println("1) Regular");
+                System.out.println("2) Savings");
+                System.out.println("3) Deposit");
+                int type;
+                type=scan.nextInt();
+                   System.out.print("Enter inintial balance  ( if balance is 0 type -1):");
+                   balance=scan.nextDouble();
+                   if(balance<0){
+                  
+                     accounts[count++]= new Account(id,name,type);
+                    }
+                   
+                   else {
+                    accounts[count++]=new Account(id,name,type,balance);
+                   }
+                   break;
+            case 2:
+                  System.out.print("Enter ID:");
+                  id=scan.nextLine();
+                    System.out.println("Enter deposit amount:");
+                    amount = scan.nextDouble();
+                    acc=find(accounts,count,id);
+                  if(acc!=null){
+                      acc.deposit(amount);
+                  }
+                  else{
+                       System.out.println("Account not found!");
+                  }
+                  break;
+            case 3: 
+                System.out.print("Enter ID:");
+                id=scan.nextLine();
+                System.out.print("Enter withdrawl amount:");
+                amount=scan.nextDouble();
+                acc=find(accounts,count,id);
+                if(acc!=null){
+                    acc.withdraw(amount);
+                }
+                else{
+                    System.out.println("Account Not found!");
+                }
+                break;
+            case 4: 
+                 System.out.print("Enter source account ID:");
+                String idsrc;
+                idsrc=scan.nextLine();
+                System.out.print("Enter destination account ID:");
+                String iddes;
+                iddes=scan.nextLine();
+                System.out.print("Enter transfer amount:");
+                amount=scan.nextDouble();
+                Account acc1=find(accounts,count,idsrc);
+                Account acc2=find(accounts,count,iddes);
+                if(acc1!=null && acc2!=null){
+                    acc1.transfer(acc2,amount);
+                }
+                else{
+                    System.out.print("No account found!");
+                }
+                break;
+            case 5:
+              for(int i=0;i<count;i++){
+                System.out.print("\n");
+                System.out.println(accounts[i]);
+                System.out.print("\n");
+              }
+              break;
+            case 6: 
+               System.out.print("Enter ID:");
+                  id=scan.nextLine();
+                    acc=find(accounts,count,id);
+                  if(acc!=null){
+                     balance=acc.getBal();
+                    System.out.print("Your current balance :"+balance);
+                   double interest = acc.getInterest();
+                    System.out.print("Your Interest balance :"+interest);
+                     balance=acc.getBal();
+                    System.out.print("Your NEW balance :"+balance);
+                     
+                  }
+                  else{
+                       System.out.println("Account not found!");
+                  }
+                  break;
+                
+            case 7:
+               break;
+            default:
+             System.out.println("Invalid choice");
+             break;
+            } 
+            for(int i=0;i<50;i++){
+                System.out.print("=");
+            }
+            System.out.print("\n");
+         }
+    }
+    private static Account find(Account[] accounts, int count , String id){
+                for(int i=0;i<count;i++){
+                    if(accounts[i].getID().equals(id)){
+                        return accounts[i];
+                    
+                    }
+                }
+                return null;
+    }
+
+}
